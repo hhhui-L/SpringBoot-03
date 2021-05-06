5bdf0796549495a548455e861801af4e3c8a64d8
hhhui-L <1184131956@qq.com>
2021-05-06 19:33:43 +0800

    05.06

---
+++ src/main/java/com/lgh/boot/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.lgh.boot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration(proxyBeanMethods = false)
+public class WebConfig {
+
+    @Bean
+    //自定义filter
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
+        hiddenHttpMethodFilter.setMethodParam("_m");
+        return hiddenHttpMethodFilter;
+    }
+
+}
--- src/main/java/com/lgh/boot/controller/HelloController.java
+++ src/main/java/com/lgh/boot/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.lgh.boot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -12,23 +10,30 @@ public class HelloController {
         return "aaaa";
     }
 
-    @RequestMapping(value = "/user",method = RequestMethod.GET)
+//    @RequestMapping(value = "/user",method = RequestMethod.GET)
+    @GetMapping("/user")
     public String getUser() {
         return "GET-张三";
     }
 
-    @RequestMapping(value = "/user",method = RequestMethod.POST)
+//    @RequestMapping(value = "/user",method = RequestMethod.POST)
+    @PostMapping("/user")
     public String saveUser() {
         return "POST-张三";
     }
 
-    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+//    @RequestMapping(value = "/user",method = RequestMethod.PUT)
+    @PutMapping("/user")
     public String putUser() {
         return "PUT-张三";
     }
 
-    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+//    @RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    @DeleteMapping("/user")
     public String deleteUser() {
         return "DELETE-张三";
     }
+
+    //扩展点：如何把_method这个名字换成我们喜欢的--->自定义filter
+
 }
